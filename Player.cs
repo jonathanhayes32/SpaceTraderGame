d68fa0275e87093ef18f60bfd6ed9d12855cccda
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-09-03 19:34:10 -0400

    git commit (Add) to Command Line

---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    public static class Player
+    {
+        //Player name
+       private static string PlayerName = "";
+
+        //Print out Game title and over view
+        public static void StartGame()
+        {
+            Console.WriteLine("Weclome to Space Trader Game!");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Space has always been a mystery to man");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("We now have the opportunity to explore it");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Thanks to technological advancements you will be embarking on this journey");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("We need you to start our new trading system with other planets");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The question is will you be successful or will failure be at your door");
+            Console.ReadKey();
+            Console.Clear();
+            NamePlayer();
+        }
+        static void NamePlayer()
+        {
+            // ask player for a name, and save it
+            Console.WriteLine("Adventurer, what is your name");
+            PlayerName = Console.ReadLine();
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Welcome " + PlayerName);
+            Console.ReadKey();
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,35 +1,18 @@
 ﻿using System;
-
+/*
+ * Space Trader
+ * by Team 7
+ *  
+ * This project is developing a Space trading game
+ */
 namespace SpaceGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string PlayerName = "";
-
-        
-            Console.WriteLine("Weclome to Space Trader Game!");
+            Player.StartGame();
             Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Space has always been a mystery to man");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("We now have the opportunity to explore it");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Thanks to technological advancements you will be embarking on this journey");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("We need you to start our new trading system with other planets");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("The question is will you be successful or will failure be at your door");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Adventurer, what is your name");
-            Console.Read();
-            PlayerName = Console.ReadLine();
         }
     }
 }
