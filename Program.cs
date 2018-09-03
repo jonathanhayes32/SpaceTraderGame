b2a0d2292ff7b1c7d33207c41101979ce8b8d746
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-09-03 18:54:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SpaceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PlayerName = "";
+
+        
+            Console.WriteLine("Weclome to Space Trader Game!");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Space has always been a mystery to man");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("We now have the opportunity to explore it");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Thanks to technological advancements you will be embarking on this journey");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("We need you to start our new trading system with other planets");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The question is will you be successful or will failure be at your door");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Adventurer, what is your name");
+            Console.Read();
+            PlayerName = Console.ReadLine();
+        }
+    }
+}
